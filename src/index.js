c182d87355d063a47eb51d3b2499c8e8134e6ac2
JADundee <Jadundee@icloud.com>
2023-11-15 10:12:19 -0700

    Updated styling

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
+
+
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
+
+window.onload = function() {
+    Particles.init({
+      selector: '.background'
+    });
+    
+  };
